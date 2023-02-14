ea03b3e58058c88546509b4e933cc96808e10dd7
Charly Delbauche <cdelbauche@jll.net>
2023-02-14 12:37:10 +0100

    modif GET

--- backend-ccharly/affichage.py
+++ backend-ccharly/affichage.py
@@ -1,32 +1,14 @@
+from flask import Flask, jsonify
 import json
-from http.server import BaseHTTPRequestHandler, HTTPServer
 
-# Définir le port sur lequel le serveur écoutera
-PORT = 8080
+app = Flask(__name__)
 
-# Définir la classe qui gère les requêtes entrantes
-class RequestHandler(BaseHTTPRequestHandler):
-    
-    def do_GET(self):
-        # Ouvrir le fichier JSON
-        with open('fichier.json', 'r') as f:
-            data = json.load(f)
-        
-        # Extraire les informations
-        id = data['id']
-        nom = data['nom']
-        nom_de_famille = data['nom_de_famille']
-        email = data['email']
-        
-        # Envoyer les informations en réponse
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        message = f'ID : {id}<br>Nom : {nom}<br>Nom de famille : {nom_de_famille}<br>Email : {email}'
-        self.wfile.write(bytes(message, 'utf8'))
+@app.route('/api/v1', methods=['GET'])
+def get_data():
+    with open('C:\\Users\\cdelbauche\\pythonhello\\charly-aws\\backend-ccharly\\data.json') as f:
+        data = json.load(f)
+        results = [{'id': item['id'], 'nom': item['nom'], 'nom_de_famille': item['nom_de_famille'], 'email': item['email']} for item in data]
+    return jsonify(results)
 
-# Lancer le serveur
 if __name__ == '__main__':
-    httpd = HTTPServer(('localhost', PORT), RequestHandler)
-    print(f'Serveur actif sur le port {PORT}')
-    httpd.serve_forever()
+    app.run(port=8080)
